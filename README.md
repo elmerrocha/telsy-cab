288969183946f19b3fc1c5372bf525c995afec02
Elmer Rocha Jaime <elmer.rocha.j@gmail.com>
2022-08-17 11:19:12 -0500

    Django with user interface

--- README.md
+++ README.md
@@ -8,10 +8,16 @@ This is what is necessary to run the project:
 - [Raspberry Pi 4 Model B 4GB](https://www.raspberrypi.com/products/raspberry-pi-4-model-b/)
 - [Raspberry Pi OS with desktop - Debian 10 Buster (May 7th 2021)](https://downloads.raspberrypi.org/raspios_armhf/images/raspios_armhf-2021-05-28/)
 - [Python 3.7.3](https://www.python.org/downloads/release/python-373/)
+- [Django 3.2.15](https://docs.djangoproject.com/en/4.0/releases/3.2.15/)
 
 Python libraries needed:
 - [pytz](https://pypi.org/project/pytz/)
 - [pyserial](https://pypi.org/project/pyserial/)
+
+## **🔧 Raspberry Pi OS - Python Libraries**
+```
+sudo pip3 install Django==3.2.15 pyserial pytz
+```
 
 ## **✒️ Autor**
 * **Elmer Rocha** - *Card firmware* - [elmerrocha](https://github.com/elmerrocha)
---
+++ telsy/cab/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from datetime import datetime
+from subprocess import Popen, CalledProcessError, PIPE
+
+# Popen dir
+RASPBERRY_PATH = './cab/raspberry/'
+MEASURE = ['python3', RASPBERRY_PATH+'berry_measurement.py']
+
+current_time = datetime.now()
+last_time = datetime.now()
+once_flarg = True
+measure_pipe = 0
+
+# Create your views here.
+def index(request):
+    ''' Index view '''
+    global last_time
+    last_time = datetime.now()
+    return render(request,'index.html')
+
+def monitor(request):
+    ''' Monitor view '''
+    global last_time, current_time, once_flarg, measure_pipe
+    current_time = datetime.now() - last_time
+    if (current_time.total_seconds() >= 45) or once_flarg:
+        try:
+            measure_pipe = Popen(MEASURE, stdout=PIPE)
+        except CalledProcessError as err:
+            print(err)
+            measure_pipe.kill()
+        if once_flarg:
+            once_flarg = False
+        last_time = datetime.now()
+    print('Time:',current_time.total_seconds())
+    return render(request,'index.html')
